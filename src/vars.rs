47df905e5e669c6f17aaab889f779840a6e2cac2
bossoncode <akinkunmiisrael41@gmail.com>
2023-02-26 14:52:59 +0100

    (((

--- src/vars.rs
+++ src/vars.rs
@@ -3,8 +3,7 @@ pub fn vars() {
     //Immutable by default
     let  name = "Akinkunmi";
 
-    //Mutable
-    let mut age =  21;
+    let age =  21;
 
-    println!("{} is {} years old", name, age);
+    println!("{} is {} years old", name, age)
 }
