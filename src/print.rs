3f041721404fbca18025600064e0fa700d8f1ab3
bossoncode <akinkunmiisrael41@gmail.com>
2023-02-26 14:37:25 +0100

    ___

--- src/print.rs
+++ src/print.rs
@@ -8,14 +8,11 @@ pub fn print(){
     println!("{} is a {} yo {}", "Akinkunmi", 21, "developer");
 
     //Positional arguments
-
     println!("{0} loves to code and {0} doesn't like {1}", "Akinkunmi", "football");
 
     //Named arguments
-
     println!("We all love {programmingLanguage}", programmingLanguage = "JavaScript");
 
     //Placeholder traits
-
     println!("Binary: {:b} Hex: {:x} Octal: {:o}", 2, 34, 16);
 }
