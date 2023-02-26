0c3ede2af64665bf88d70aa932d1b51d6211985f
bossoncode <akinkunmiisrael41@gmail.com>
2023-02-26 22:24:13 +0100

    ___

--- src/types.rs
+++ src/types.rs
@@ -1,13 +1,13 @@
 pub fn types(){
 
     //Default i32
-    let num = 69;
+    let _num = 69;
 
     //Default f64
-    let fl = 6.9;
+    let _fl = 6.9;
 
     //Explicit type
-    let exp:i128 = 2345678987654325678909876;
+    let _exp:i128 = 2345678987654325678909876;
 
     //Find max size
     println!("i32:{} | i64:{} | i128:{}", std::i32::MAX, std::i64::MAX, std::i128::MAX);
