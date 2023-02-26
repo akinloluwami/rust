7cde5fe0a9a610c491713ddba7163b5cc6dcf7f4
bossoncode <akinkunmiisrael41@gmail.com>
2023-02-26 14:27:31 +0100

    added comment

--- src/print.rs
+++ src/print.rs
@@ -1,6 +1,8 @@
 pub fn print(){
+    //Print to console
     println!("Hey, babe.");
 
+    //Basic formatting
     println!("Do you have {} GB?", 69);
 
     println!("{} is a {} yo {}", "Akinkunmi", 21, "developer")
