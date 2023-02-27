47bf55d7f577a3749d78c452ad1275966a0b46f1
bossoncode <akinkunmiisrael41@gmail.com>
2023-02-27 19:02:17 +0100

    just testing if I still remember this shii

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::path;
+
 // mod print;
 // mod vars;
 // mod types;
@@ -7,5 +9,9 @@ fn main() {
     // print::print();
     // vars::vars();
     // types::types();
-    strings::strings();
+    // strings::strings();
+
+    let url = "https://www.npmjs.com/package/electron-context-menu";
+    let paths: Vec<&str> = url.split("/").collect();
+    println!("{:?}", paths);
 }
