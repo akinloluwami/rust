14c28b3db3c94b1a27875c396b3c195992e23a18
bossoncode <akinkunmiisrael41@gmail.com>
2023-02-27 19:02:35 +0100

    I remember

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::path;
-
 // mod print;
 // mod vars;
 // mod types;
@@ -9,9 +7,5 @@ fn main() {
     // print::print();
     // vars::vars();
     // types::types();
-    // strings::strings();
-
-    let url = "https://www.npmjs.com/package/electron-context-menu";
-    let paths: Vec<&str> = url.split("/").collect();
-    println!("{:?}", paths);
+    strings::strings();
 }
