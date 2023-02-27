0ede0f115519e3cb73cb732ec97e4266fa2d1fb9
bossoncode <akinkunmiisrael41@gmail.com>
2023-02-27 11:23:47 +0100

    split

--- src/strings.rs
+++ src/strings.rs
@@ -22,10 +22,23 @@ pub fn strings(){
     // println!("Is empty: {}", text.is_empty());
 
     //Contain
-    println!("Contains 'a': {}", text.contains("a"));
+    // println!("Contains 'a': {}", text.contains("a"));
 
 
     //Replace
-    println!("{}", text.replace("i", "a"))
+    // println!("{}", text.replace("i", "a"));
+
+
+    //Loop through strig by whitespace
+    let voice = "Even if you're not ready for the day, it cannot always be night";
+    for word in voice.split_whitespace(){
+        // println!("{}",word);
+    }
+
+    //Split by character
+    let url = "https://marcchen.hashnode.dev/building-desktop-applications-with-tauri-nextjs-firebase";
+    let parts: Vec<&str> = url.split("/").collect();
+    let path : Vec<&str> =  parts[3].split("-").collect(); 
+    println!("{:?}", path);
 
 }
